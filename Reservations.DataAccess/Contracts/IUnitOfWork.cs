3b59beec526ca64847f81df50c9c0eb481d5dedf
Yaser Guerra Martínez <yaserguerra@gmail.com>
2018-04-18 19:50:33 -0400

    (+) Add UnitOfWork

---
+++ Reservations.DataAccess/Contracts/IUnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using Reservations.Core.Entities;
+
+namespace Reservations.DataAccess.Contracts
+{
+
+    /// <inheritdoc />
+    /// <summary>
+    ///     The UnitOfWork interface.
+    /// </summary>
+    public interface IUnitOfWork : IDisposable
+    {
+        #region Public Properties
+
+        /// <summary>
+        ///     Gets the contacts.
+        /// </summary>
+        IRepository<Contact> Contacts { get; }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// The save changes.
+        /// </summary>
+        void SaveChanges();
+
+        #endregion
+    }
+}
---
+++ Reservations.DataAccess/UnitOfWorkImpl/SqlUnitOfWork.cs
@@ -0,0 +1,79 @@
+﻿
+using Reservations.Core.Entities;
+using Reservations.DataAccess.Contracts;
+using Reservations.DataAccess.DataContext;
+using Reservations.DataAccess.RepositoryImpl;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+
+namespace Reservations.DataAccess.UnitOfWorkImpl
+{
+
+    /// <inheritdoc />
+    /// <summary>
+    ///     The sql unit of work.
+    /// </summary>
+    public class SqlUnitOfWork : IUnitOfWork
+    {
+        #region Fields
+
+        /// <summary>
+        ///     The contacts.
+        /// </summary>
+        private IRepository<Contact> contacts;
+
+        #endregion
+
+        #region Constructors and Desctructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="SqlUnitOfWork" /> class.
+        /// </summary>
+        public SqlUnitOfWork()
+        {
+            var dataContext = new EfDbContext();
+            var adapter = (IObjectContextAdapter)dataContext;
+            this.Context = adapter.ObjectContext;
+        }
+
+        #endregion
+
+        #region Private Properties
+
+        /// <summary>
+        ///     Gets the context.
+        /// </summary>
+        private ObjectContext Context { get; }
+
+        #endregion
+
+        #region IUnitOfWork Members
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     The contacts.
+        /// </summary>
+        public IRepository<Contact> Contacts =>
+            this.contacts ?? (this.contacts = new SqlRepository<Contact>(this.Context));
+
+        /// <inheritdoc />
+        /// <summary>
+        /// The save changes.
+        /// </summary>
+        public void SaveChanges()
+        {
+            this.Context.SaveChanges();
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     The dispose.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Context?.Dispose();
+        }
+
+        #endregion
+    }
+}
