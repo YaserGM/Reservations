f50f39b4c7d74fbdf79596e0040dcd36d4a396a6
Yaser Guerra Martínez <yaserguerra@gmail.com>
2018-04-22 19:59:07 -0400

    Add class enums in proyecto core

---
+++ Reservations.Core/Enums/ContactTypeEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservations.Core.Enums
+{
+    public enum ContactTypeEnum
+    {
+        Student = 1,
+        Teacher = 2,
+        Engineer = 3,
+        Economic = 4,
+        Actor = 5
+    }
+}
---
+++ Reservations.Core/Enums/OrderByEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservations.Core.Enums
+{
+    public enum OrderByEnum
+    {
+        [Description("By Date Ascending")] DateAscending = 1,
+        [Description("By Date Descending")] DateDescending = 2,
+        [Description("By Alphabetic Ascending")] AlphabeticAscending = 3,
+        [Description("By Alphabetic Descending")] AlphabeticDescending = 4,
+        Ranking = 5
+    }
+}
