478385446a56259c253e079dcef197e97e6e47c9
Yaser Guerra Martínez <yaserguerra@gmail.com>
2018-04-22 11:51:34 -0400

    (+) Add config package AutoMapper and Autofac

---
+++ Reservations.App/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Reservations.App.Models;
+using Reservations.Core.Entities;
+
+namespace Reservations.App
+{
+    /// <summary>
+    /// The auto mapper config.
+    /// </summary>
+    public class AutoMapperConfig
+    {
+        #region Public Static Methods
+
+        /// <summary>
+        /// The initialize mapper.
+        /// </summary>
+        public static void InitializeMapper()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Contact, ContactDto>();
+                cfg.CreateMap<Reservation, ReservationDto>();
+            });
+        }
+
+        #endregion
+    }
+}
---
+++ Reservations.App/App_Start/AutofacConfig.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Reservations.Business.Services.Contacts;
+using Reservations.Business.Services.Reservations;
+using Reservations.DataAccess.Contracts;
+using Reservations.DataAccess.UnitOfWorkImpl;
+
+namespace Reservations.App
+{
+    public class AutofacConfig
+    {
+        #region Public Methods
+
+        public static void Initialize()
+        {
+            var builder = new ContainerBuilder();
+
+            // MVC - Register your MVC controllers.
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // MVC - OPTIONAL: Register model binders that require DI.
+            builder.RegisterModelBinders(typeof(MvcApplication).Assembly);
+            builder.RegisterModelBinderProvider();
+
+            // MVC - OPTIONAL: Register web abstractions like HttpContextBase.
+            builder.RegisterModule<AutofacWebTypesModule>();
+
+            // MVC - OPTIONAL: Enable property injection in view pages.
+            builder.RegisterSource(new ViewRegistrationSource());
+
+            // MVC - OPTIONAL: Enable property injection into action filters.
+            builder.RegisterFilterProvider();
+
+            // Register application dependencies.
+            ///builder.RegisterType<DataContextLocal>().As<DataContextLocal>();
+
+            builder.RegisterType<SqlUnitOfWork>().As<IUnitOfWork>();
+            builder.RegisterType<ContactService>().As<IContactService>();
+            builder.RegisterType<ReservationService>().As<IReservationService>();
+            
+
+            // MVC - Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+        #endregion
+    }
+}
