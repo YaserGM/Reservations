098b8dd48432891f1f5d73ce1011b6fb09725150
Yaser Guerra Martínez <yaserguerra@gmail.com>
2018-04-22 11:53:32 -0400

    Modifid config to add load components: quill and start-rating

--- Reservations.App/App_Start/BundleConfig.cs
+++ Reservations.App/App_Start/BundleConfig.cs
@@ -9,23 +9,31 @@ namespace Reservations.App
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/bootstrap.css", "~/Content/site.css",
+                    "~/Content/Index.css", "~/Content/create_edit.css",
+                    "~/Content/quill/quill.snow.css", "~/Content/quill/quill.bubble.css",
+                    "~/Content/star-rating.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modules_js")
+                .Include("~/Scripts/quill/quill.min.js",
+                "~/Scripts/knockout-latest.js", "~/Scripts/knockout-latest.debug.js",
+                    "~/Scripts/star-rating.js"));
         }
     }
 }
--- Reservations.App/App_Start/RouteConfig.cs
+++ Reservations.App/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Reservations.App
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Reservations", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
