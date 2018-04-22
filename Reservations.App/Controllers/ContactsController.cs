7b42233ea69522011742009aaab3de813b255831
Yaser Guerra Martínez <yaserguerra@gmail.com>
2018-04-22 19:54:58 -0400

    Add controller Contacts and Reservations

---
+++ Reservations.App/Controllers/ContactsController.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper;
+using Reservations.App.Models;
+using Reservations.Business.Dto;
+using Reservations.Business.Services.Contacts;
+using Reservations.Core.Entities;
+using Reservations.DataAccess.DataContext;
+
+namespace Reservations.App.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private readonly IContactService contactService;
+
+        //Quitar
+        private EfDbContext db = new EfDbContext();
+
+
+        public ContactsController(IContactService contactService)
+        {
+            this.contactService = contactService;
+        }
+
+
+        // GET: Contacts
+        public ActionResult Index()
+        {
+            /*const int MaxCount = 5;
+            var skipCount = (page - 1) * MaxCount;
+            var response = this.contactService.GetAll(new PaginationResult(skipCount, MaxCount));
+            var entityList = response.Items;
+            var dtoList = Mapper.Map<List<Contact>, List<ContactDto>>(entityList);
+            this.ViewBag.PageCount = (int)Math.Ceiling((double)response.SourceTotal / MaxCount);
+            this.ViewBag.CurrentPage = page;
+            return this.View(dtoList);*/
+
+
+            var response = this.contactService.GetAll();
+            var dtoList = Mapper.Map<List<Contact>, List<ContactDto>>(response.Items);
+
+            return this.View(dtoList);
+
+
+            //return View(db.Contacts.ToList());
+        }
+
+        // GET: Contacts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(contact);
+        }
+
+        // GET: Contacts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contacts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,PhoneNumber,Birthdate,Contacttype")]
+            Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        // GET: Contacts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(contact);
+        }
+
+        // POST: Contacts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,PhoneNumber,Birthdate,Contacttype")]
+            Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contact).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contact);
+        }
+
+        // GET: Contacts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(contact);
+        }
+
+        // POST: Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            db.Contacts.Remove(contact);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public JsonResult GetById(int id = 0)
+        {
+            if (id == 0)
+            {
+                return null;
+            }
+
+            var contact = this.contactService.Get(id);
+            var contactDto = Mapper.Map<ContactDto>(contact);
+
+
+            return this.Json(contact, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetByName(string name)
+        {
+            if (name == null)
+            {
+                return null;
+            }
+
+            var contact = this.contactService.Get(name);
+            var contactDto = new ContactDto();
+            if (contact != null)
+            {
+                contactDto = Mapper.Map<ContactDto>(contact);
+                contactDto.BirthdateText = contactDto.BirthdateString();
+            }
+
+            return this.Json(contactDto, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult FindByName(string name)
+        {
+            if (name == null)
+            {
+                return null;
+            }
+
+            var contact = this.contactService.Get(name);
+            var contactDto = new ContactDto();
+            if (contact != null)
+            {
+                contactDto = Mapper.Map<ContactDto>(contact);
+                contactDto.BirthdateText = contactDto.BirthdateString();
+            }
+
+            ViewBag.Contact = contactDto;
+
+            return RedirectToAction("Edit");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Reservations.App/Controllers/ReservationsController.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper;
+using Reservations.App.Models;
+using Reservations.Business.Dto;
+using Reservations.Business.Services.Contacts;
+using Reservations.Business.Services.Reservations;
+using Reservations.Core.Entities;
+using Reservations.Core.Enums;
+using Reservations.DataAccess.DataContext;
+
+namespace Reservations.App.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        private readonly IReservationService _reservationService;
+        private readonly IContactService _contactService;
+
+
+        public ReservationsController(IReservationService reservationService, IContactService contactService)
+        {
+            this._reservationService = reservationService;
+            this._contactService = contactService;
+        }
+
+        // GET: Reservations
+        public ActionResult Index(int page = 1, string sort = "1")
+        {
+            var reservationDetoList = ReservationDetoList(page, sort);
+
+            return this.View(reservationDetoList);
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(string sort, int page = 1)
+        {
+            var reservationDetoList = ReservationDetoList(page, sort);
+
+            return this.View(reservationDetoList);
+        }
+
+        // GET: Reservations/Create
+        public ActionResult Create(int page, string sort)
+        {
+            this.ViewBag.page = page;
+            this.ViewBag.sort = sort;
+            this.ViewBag.contactTypeList = Helper.EnumHelper.ToListSelectListItem<ContactTypeEnum>();
+            var reservationDto = new ReservationDto()
+            {
+                Contact = new Contact()
+            };
+            return View(reservationDto);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ReservationDto reservationDto, int page, string sort)
+        {
+            this.ViewBag.page = page;
+            this.ViewBag.sort = sort;
+
+            if (ModelState.IsValid)
+            {
+                reservationDto.CreateDate = DateTime.Now;
+                try
+                {
+                    var reservation = Mapper.Map<Reservation>(reservationDto);
+                    if (reservationDto.Contact.Id > 0)
+                    {
+                        this._contactService.Update(reservationDto.Contact);
+                    }
+                    else
+                    {
+                        Contact newContact = this._contactService.Add(reservationDto.Contact);
+                        reservationDto.Contact = newContact;
+                    }
+
+                    reservation.ContactId = reservation.Contact.Id;
+                    this._reservationService.Add(reservation);
+                    return RedirectToAction("Index", new {page, sort});
+                }
+                catch
+                {
+                    return this.View();
+                }
+            }
+            this.ViewBag.contactTypeList = Helper.EnumHelper.ToListSelectListItem<ContactTypeEnum>();
+            return View(reservationDto);
+        }
+
+        // GET: Reservations/Edit/5
+        public ActionResult Edit(int page, string sort, int id = 0)
+        {
+            this.ViewBag.page = page;
+            this.ViewBag.sort = sort;
+            if (id == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            this.ViewBag.contactTypeList = Helper.EnumHelper.ToListSelectListItem<ContactTypeEnum>();
+            var res = this._reservationService.Get(id);
+            var reservationDto = Mapper.Map<ReservationDto>(res);
+            return this.View(reservationDto);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ReservationDto reservationDto, int page, string sort)
+        {
+            this.ViewBag.page = page;
+            this.ViewBag.sort = sort;
+            if (!this.ModelState.IsValid)
+            {
+                return this.View();
+            }
+
+            try
+            {
+                var reservation = Mapper.Map<Reservation>(reservationDto);
+                if (reservationDto.Contact.Id > 0)
+                {
+                    this._contactService.Update(reservationDto.Contact);
+                }
+                else
+                {
+                    Contact newContact = this._contactService.Add(reservationDto.Contact);
+                    reservationDto.Contact = newContact;
+                }
+
+                this._reservationService.Update(reservation);
+                return RedirectToAction("Index", new {page, sort});
+            }
+            catch
+            {
+                return this.View();
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult UpdateRating(int id, string value)
+        {
+            if (id == 0)
+            {
+                return null;
+            }
+
+            var reservation = this._reservationService.Get(id);
+            if (reservation != null)
+            {
+                reservation.RanKing = Double.Parse(value);
+                this._reservationService.Update(reservation);
+            }
+
+            return null;
+        }
+
+        public JsonResult GetById(int id = 0)
+        {
+            if (id == 0)
+            {
+                return null;
+            }
+
+            var reservation = this._reservationService.Get(id);
+            var reservationDto = Mapper.Map<ReservationDto>(reservation);
+
+            ContactDto contact = Mapper.Map<ContactDto>(reservationDto.Contact);
+            contact.BirthdateText = contact.BirthdateString();
+            reservationDto.Contact = null;
+
+            return this.Json(new List<Object>
+                {
+                    reservationDto,
+                    contact
+                }
+                , JsonRequestBehavior.AllowGet);
+        }
+
+        private List<ReservationDto> ReservationDetoList(int page, string sort)
+        {
+            const int maxPage = 5;
+            var skipCount = (page - 1) * maxPage;
+            OrderByEnum.TryParse(sort, out OrderByEnum sortBy);
+
+            var response =
+                this._reservationService.OrderBy(sortBy, new PageResult(skipCount, maxPage));
+
+            var reservationDetoList = Mapper.Map<List<Reservation>, List<ReservationDto>>(response.Items);
+            this.ViewBag.PageCount = (int) Math.Ceiling((double) response.SourceTotal / maxPage);
+            this.ViewBag.page = page;
+            this.ViewBag.sort = sort;
+            this.ViewBag.listSorts = Helper.EnumHelper.ToListSelectListItem<OrderByEnum>();
+            return reservationDetoList;
+        }
+    }
+}
