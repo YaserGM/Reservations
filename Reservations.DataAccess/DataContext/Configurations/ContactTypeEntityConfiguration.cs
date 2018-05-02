affd2b0262720880469db541c626607e78497f2b
Yaser Guerra Martínez <yaserguerra@gmail.com>
2018-05-02 04:43:17 -0400

    Add ContactTypeEntityConfiguration by api fluent

---
+++ Reservations.DataAccess/DataContext/Configurations/ContactTypeEntityConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Reservations.Core.Entities;
+
+namespace Reservations.DataAccess.DataContext.Configurations
+{
+    public class ContactTypeEntityConfiguration : EntityTypeConfiguration<ContactType>
+    {
+        public ContactTypeEntityConfiguration()
+        {
+            this.Property(t => t.Description).IsRequired()
+                .HasMaxLength(30);
+            this.HasIndex(t => t.Description).IsUnique();
+        }
+    }
+}
